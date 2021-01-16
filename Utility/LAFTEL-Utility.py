9c7236b456e2900c835a724138787c6d78bf3034
Nitro <nitro0@naver.com>
2021-01-17 00:17:41 +0900

    Update some comment

--- Utility/LAFTEL-Utility.py
+++ Utility/LAFTEL-Utility.py
@@ -10,8 +10,8 @@ token = config.get('LAFTEL', 'token')
 user_id = config.get('LAFTEL', 'user_id')
 
 headers = {
-    'laftel':'TeJava',
-    'authorization':f'Token {token}',
+    'laftel': 'TeJava',
+    'authorization': f'Token {token}',
 }
 
 def getUserInfo():
@@ -36,9 +36,10 @@ def searchItem(keyword):
 
 def getWishList(user_id):
     """
-    Get wish items from the LAFTEL DB and return the JSON formatted list file.
+    Get wish list from the LAFTEL API and return the JSON object.
      - ``user_id`` is a unique integer value that identifies the user.
      - Return ``JSON`` that includes the wish list.
+     - Return ``status code`` of the API request.
     """
     response = requests.get(f'https://laftel.net/api/v1.0/users/{user_id}/rate/is_wish/', headers=headers)
     data = json.loads(response.text)
@@ -47,30 +48,44 @@ def getWishList(user_id):
 def editWishItem(item_id, wish):
     """
     Modify the status of the wish item.
-     - ``item_id`` is a unique integer id value of an item (or Anime).
-     - ``wish`` is a boolean type value that indicates the status of the item that you like to change. 
-     - Return ``status code`` of the API request. 
+     - ``item_id`` is a unique integer id value of an item.
+     - ``wish`` is a boolean type value that indicates the status of the item that you like to change.
+     - Return ``status code`` of the API request.
     """
     response = requests.post(f'https://laftel.net/api/v1.0/items/{item_id}/wish/', headers=headers, data={ 'is_wish': wish })
     return response.status_code
 
-def clearWishList(user_id):
+def clearWishList(user_id, log=True):
     """
     Change the status of wish items in the user's wish lists to false.
      - ``user_id`` is a unique integer value that identifies the user.
+     - If ``log`` is True, the function will print the item id and status code.
     """
     wish = getWishList(user_id)
     for key, value in wish.items():
         if value:
             code = editWishItem(key, False)
-            print(f'{key}:{code}')
+            if log: print(f'{key}:{code}')
 
 def getItemDetail(item_id):
+    """
+    Get detailed information about the item.
+     - ``item_id`` is a unique integer id value of an item.
+     - Return ``JSON`` that includes detailed information about the item.
+     - Return ``status code`` of the API request. 
+    """
     response = requests.get(f'https://laftel.net/api/v1.0/items/{item_id}/detail/', headers=headers)
     data = json.loads(response.text)
     return data, response.status_code
 
 def getItemEpisodes(item_id, limit=1000):
+    """
+    Get an episode list of an item that includes detailed information about each episode.
+     - ``item_id`` is a unique integer id value of an item.
+     - ``limit`` is the maximum number of the episode that will be returned.
+     - Return ``JSON`` that includes detailed episode information about the item.
+     - Return ``status code`` of the API request. 
+    """
     params = {
         'item_id': item_id,
         'limit': limit
@@ -80,6 +95,12 @@ def getItemEpisodes(item_id, limit=1000):
     return data, response.status_code
 
 def getRelatedItem(item_id):
+    """
+    Get a list of a related item. This includes the series of the current item or the item that has similar characteristics.
+     - ``item_id`` is a unique integer id value of an item.
+     - Return ``JSON`` that includes detailed episode information about the item.
+     - Return ``status code`` of the API request. 
+    """
     response = requests.get(f'https://laftel.net/api/v1.0/items/{item_id}/related/', headers=headers)
     data = json.loads(response.text)
     return data, response.status_code
